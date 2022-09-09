dc891a296d227348699854b71f25b2520d088140
Arslan <kenan.arslan@rabobank.nl>
2022-09-09 16:41:51 +0200

    simplified boolean  = true

--- src/company/a/b/c/kata/BooleanToString.java
+++ src/company/a/b/c/kata/BooleanToString.java
@@ -3,7 +3,7 @@ package company.a.b.c.kata;
 public class BooleanToString {
 
     public static String convert(boolean b) {
-        if (b == true) {
+        if (b) {
             return "true";
         }
         return "false";
