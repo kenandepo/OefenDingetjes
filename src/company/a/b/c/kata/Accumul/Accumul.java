92865c1c16d8d2cd8dccd907a61770905692db6c
Arslan <kenan.arslan@rabobank.nl>
2022-09-11 21:20:12 +0200

    MathOperation / todo

---
+++ src/company/a/b/c/kata/Accumul/Accumul.java
@@ -0,0 +1,25 @@
+package company.a.b.c.kata.Accumul;
+
+//his time no story, no theory. The examples below show you how to write function accum:
+//
+//Examples:
+//accum("abcd") -> "A-Bb-Ccc-Dddd"
+//accum("RqaEzty") -> "R-Qq-Aaa-Eeee-Zzzzz-Tttttt-Yyyyyyy"
+//accum("cwAt") -> "C-Ww-Aaa-Tttt"
+//The parameter of accum is a string which includes only letters from a..z and A..Z.
+public class Accumul {
+
+    public static String accum(String s) {
+        final String input = s;
+        final int length = s.length();
+
+        for (int i = 0; i < length; i++) {
+            String temp = "";
+            String result = "";
+            for (int j = 0; j < i; j++) {
+                input.charAt(i);
+            result=//todo
+            }
+        }
+    }
+}
