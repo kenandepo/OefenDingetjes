dca21f34a7359bbf18f4b0d2278d382838d4352f
Arslan <kenan.arslan@rabobank.nl>
2022-09-26 15:22:23 +0200

    IntToString

---
+++ src/company/a/b/c/kata/inttostring/IntToString.java
@@ -0,0 +1,18 @@
+package company.a.b.c.kata.inttostring;
+
+public class IntToString {
+    public static String numberToString(int num) {
+        return String.valueOf(num);
+    }
+}
+//import org.junit.Test;
+//import static org.junit.Assert.assertEquals;
+//
+//public class NumberStringExampleTests {
+//  @Test
+//  public void tests() {
+//    assertEquals("67", Kata.numberToString(67));
+//    assertEquals("123", Kata.numberToString(123));
+//    assertEquals("999", Kata.numberToString(999));
+//  }
+//}
---
+++ src/company/a/b/c/kata/square/Square.java
@@ -0,0 +1,16 @@
+package company.a.b.c.kata.square;
+
+public class Square {
+
+    public static void main(String[] args) {
+        isSquare(3);
+    }
+
+    public static boolean isSquare(int n) {
+        final int result = (n / n);
+        System.out.println(result);
+        return false;
+    }
+}
+
+
