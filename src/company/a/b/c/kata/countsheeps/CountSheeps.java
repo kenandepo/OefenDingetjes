793b65629e092daab091eb68e740fdfec3935219
Arslan <kenan.arslan@rabobank.nl>
2022-09-26 15:43:01 +0200

    countSheeps

--- src/company/a/b/c/kata/countsheeps/CountSheeps.java
+++ src/company/a/b/c/kata/countsheeps/CountSheeps.java
@@ -2,17 +2,17 @@ package company.a.b.c.kata.countsheeps;
 //Consider an array/list of sheep where some sheep may be missing from their place. We need a function that counts the number of sheep present in the array (true means present).
 public class CountSheeps {
 
-    public int countSheeps(Boolean[] arrayOfSheeps) {
-        int totalNum = 0;
+        public int countSheeps(Boolean[] arrayOfSheeps) {
+            int totalNum = 0;
 
-        for (int i = 0; i < arrayOfSheeps.length; i++) {
-            if (true == (arrayOfSheeps[i])) {
-                totalNum = totalNum + 1;
+            for (int i = 0; i < arrayOfSheeps.length; i++) {
+                if (  arrayOfSheeps[i]!=null && true == (arrayOfSheeps[i])) {
+                    totalNum = totalNum + 1;
+                }
+
             }
-
+            return totalNum;
         }
-        return totalNum;
-    }
 }
 //import org.junit.Test;
 //import static org.junit.Assert.assertEquals;
