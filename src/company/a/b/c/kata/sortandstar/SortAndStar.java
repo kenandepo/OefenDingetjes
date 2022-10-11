6e3eb49ad768dcd4bb3b998f59654d0675648827
Arslan <kenan.arslan@rabobank.nl>
2022-10-11 20:34:16 +0200

    SortAndStar refactored String.join + split

--- src/company/a/b/c/kata/sortandstar/SortAndStar.java
+++ src/company/a/b/c/kata/sortandstar/SortAndStar.java
@@ -5,21 +5,7 @@ import java.util.Arrays;
 public class SortAndStar {
 
     public static String twoSort(String[] s) {
-
         Arrays.sort(s);
-
-        String firstStringInArray = s[0];
-
-        StringBuilder stringBuilder = new StringBuilder();
-
-        for (int i = 0; i < firstStringInArray.length(); i++) {
-            stringBuilder.append(s[0].charAt(i) + "***");
-        }
-
-        return String.valueOf(stringBuilder.substring(0, stringBuilder.length() - 3));
+        return String.join("***", s[0].split(""));
     }
-    //better sollution - >
-//     java.util.Arrays.sort(s);
-//
-//        return String.join("***",s[0].split(""));
 }
