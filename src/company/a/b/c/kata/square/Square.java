1cd86294224681bc3e8fb215cd9fb00f5a1f1b14
Arslan <kenan.arslan@rabobank.nl>
2022-11-09 11:17:39 +0100

    Square

--- src/company/a/b/c/kata/square/Square.java
+++ src/company/a/b/c/kata/square/Square.java
@@ -2,15 +2,8 @@ package company.a.b.c.kata.square;
 
 public class Square {
 
-    public static void main(String[] args) {
-        isSquare(3);
-    }
-
     public static boolean isSquare(int n) {
-        final int result = (n / n);
-        System.out.println(result);
-        return false;
+        final int sqrt = (int) Math.sqrt(n);
+        return sqrt*sqrt==n;
     }
 }
-
-
