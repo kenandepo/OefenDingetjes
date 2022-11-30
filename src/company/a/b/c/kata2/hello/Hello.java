daad6bd10e5077a1da98cec7f5bc8e126e29fe13
Arslan <kenan.arslan@rabobank.nl>
2022-11-30 11:54:16 +0100

    return String.format("Hello, %s! Welcome to %s, %s!",String.join(" ", name),city,state);

--- src/company/a/b/c/kata2/hello/Hello.java
+++ src/company/a/b/c/kata2/hello/Hello.java
@@ -3,7 +3,14 @@ package company.a.b.c.kata2.hello;
 public class Hello {
 
     public String sayHello(String[] name, String city, String state) {
-
-        return "Hello, "+ name[0] + " " + name[1] + "! Welcome to " + city + ", " + state + "!";
+        String totalName = "";
+        for (int i = 0; i < name.length; i++) {
+            totalName=totalName+" "+name[i];
+        }
+        final String substring = totalName.substring(1);
+        return "Hello, " + substring + "! Welcome to " + city + ", " + state + "!";
     }
 }
+
+//other solution
+//    return String.format("Hello, %s! Welcome to %s, %s!",String.join(" ", name),city,state);
