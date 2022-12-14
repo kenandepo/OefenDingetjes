a176820e21205ec3b9e356fcc9df51afd4b4d064
kenanarslan <kenan.depo@gmail.com>
2022-12-14 10:40:35 +0100

    another way of writing FixCase string.chars.filter(Character::isLowerCase).count();

--- src/company/a/b/c/kata2/fixcase/FixCase.java
+++ src/company/a/b/c/kata2/fixcase/FixCase.java
@@ -2,20 +2,6 @@ package company.a.b.c.kata2.fixcase;
 
 public class FixCase {
     public static String solve(final String str) {
-        int numberOfCapitalCharactersInInput = 0;
-
-        for (int i = 0; i < str.length(); i++) {
-
-            if (Character.isUpperCase(str.charAt(i))) {
-                numberOfCapitalCharactersInInput++;
-            }
-        }
-        if (numberOfCapitalCharactersInInput == 0) {
-            return str.toLowerCase();
-        }
-        if (numberOfCapitalCharactersInInput > str.length() / 2) {
-            return str.toUpperCase();
-        }
-        return str.toLowerCase();
+        return str.chars().filter(Character::isLowerCase).count() >= str.chars().filter(Character::isUpperCase).count() ? str.toLowerCase() : str.toUpperCase();
     }
 }
