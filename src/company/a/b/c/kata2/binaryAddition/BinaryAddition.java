e19153a507c9c441cf69019cdf48a104770b3d8d
kenanarslan <kenan.depo@gmail.com>
2023-01-24 18:59:26 +0100

    BinaryAddition

---
+++ src/company/a/b/c/kata2/binaryAddition/BinaryAddition.java
@@ -0,0 +1,8 @@
+package company.a.b.c.kata2.binaryAddition;
+
+public class BinaryAddition {
+
+    public static String binaryAddition(int a, int b) {
+        return Integer.toBinaryString(a + b);
+    }
+}
