c1fe8f3f01a06e123dc1deba2334ba335919b085
kenanarslan <kenan.depo@gmail.com>
2023-01-27 17:11:19 +0100

    Zwy0o

--- src/company/a/b/c/kata3/greetme/GreetMe.java
+++ src/company/a/b/c/kata3/greetme/GreetMe.java
@@ -2,6 +2,6 @@ package company.a.b.c.kata3.greetme;
 
 public class GreetMe {
     public static String greet(String name) {
-        return "Hello " + name.substring(0, 1).toUpperCase() + name.substring(1) + "!";
+        return "Hello " + name.substring(0, 1).toUpperCase() + name.substring(1).toLowerCase() + "!";
     }
 }
